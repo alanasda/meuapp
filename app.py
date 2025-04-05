cd52c2285e5a68f511c2da842f5ac734a92aeff3
alanasda <ferreiramateuss000@gmail.com>
2025-04-05 13:30:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -29,5 +29,8 @@ def webhook():
     except Exception as e:
         return {'error': str(e)}, 500
 
+import os
+
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))  # usa a porta da Render se existir
+    app.run(host='0.0.0.0', port=port)
